930056ef48b513a02859d468b7b71c0194758a31
EvertonSouza12 <souza1287@live.com>
2023-05-27 17:13:51 -0300

    IMC popup

---
+++ main.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modelMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+    
+    const weight  = inputWeight.value
+    const height = inputHeight.value
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+    modelMessage.innerText = message
+    modalWrapper.classList.add('open')
+}
+
+modalBtnClose.onclick = () => {
+    modalWrapper.classList.remove('open')
+}
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
+}
