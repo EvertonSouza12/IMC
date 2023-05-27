94f7b37944ce647973b824f6689b07264fe7e44c
EvertonSouza12 <souza1287@live.com>
2023-05-27 18:23:44 -0300

    error.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -11,13 +12,23 @@ form.onsubmit = function(event) {
     
     const weight  = inputWeight.value
     const height = inputHeight.value
+
+    const showAlertError = notANumber(weight) || notANumber(height)
+    if(showAlertError){
+            AlertError.open()
+        return;
+    }
+    AlertError.close()
+
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
     modal.message.innerText = message
     modal.open()
 }
 
-
+ function notANumber(value) {
+    return isNaN(value) || value == ""
+ }
 
 function IMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
