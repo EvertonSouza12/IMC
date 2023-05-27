35265aaad9e7500e64d3b209dbe47106282d4278
EvertonSouza12 <souza1287@live.com>
2023-05-27 18:05:45 -0300

    codigo modular

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
+import { modal } from "./modal.js"
+
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-const modal = {
-
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-    open() {
-        modal.wrapper.classList.add('open')
-    },
-    close() {
-        modal.wrapper.classList.remove('open')
-    }
-}
 
 
 
@@ -28,9 +17,7 @@ form.onsubmit = function(event) {
     modal.open()
 }
 
-modal.buttonClose.onclick = () => {
-    modal.close()
-}
+
 
 function IMC(weight, height) {
     return (weight / ((height / 100) ** 2)).toFixed(2)
